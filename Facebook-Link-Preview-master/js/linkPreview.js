a78c8c4ecc8a8e67ef1f265d3391bdb09639b15e
emadtawadros <emad.tawadros@gmail.com>
2014-03-03 13:39:33 -0800

    Update linkPreview.js

--- Facebook-Link-Preview-master/js/linkPreview.js
+++ Facebook-Link-Preview-master/js/linkPreview.js
@@ -442,7 +442,7 @@
                     iframeId = iframeId[0];
 			
 			
-                    var richComment = '<div class="previewPosted" style=""><div class="previewTextPosted"> '+itemText+' </div> '+itemIframe+' <div class="previewImagesPosted"><div class="previewImagePosted"><img id="img_'+iframeId+'" src="'+itemImage+'" class="imgIframe" style="width: 130px; height: auto; float: left;"><span class="videoPostPlay"></span></div></div><div class="previewContentPosted"><div class="previewTitlePosted" id="pTP_'+iframeId+'" style="width: 355px">'+itemTitle+'</div><div class="previewUrlPosted">'+itemCanonicalUrl+'</div><div class="previewDescriptionPosted" id="pDP_'+iframeId+'" style="width: 355px"> <span id="previewSpanDescription">'+itemDescription+'</textarea></div></div><div style="clear: both"></div></div>';
+                    var richComment = '<div class="previewPosted" style="">'+itemIframe+' <div class="previewImagesPosted"><div class="previewImagePosted"><img id="img_'+iframeId+'" src="'+itemImage+'" class="imgIframe" style="width: 130px; height: auto; float: left;"><span class="videoPostPlay"></span></div></div><div class="previewContentPosted"><div class="previewTitlePosted" id="pTP_'+iframeId+'" style="width: 355px">'+itemTitle+'</div><div class="previewUrlPosted">'+itemCanonicalUrl+'</div><div class="previewDescriptionPosted" id="pDP_'+iframeId+'" style="width: 355px"> <span id="previewSpanDescription">'+itemDescription+'</textarea></div></div><div style="clear: both"></div></div>';
 
                    opts.component.api(opts.objectID + '/comments', 'post', {
             		"description": itemText,
@@ -484,7 +484,7 @@
                 else
                 {
                 	
-                	var richComment = '<div class="previewPosted" style=""><div class="previewTextPosted"> '+itemText+'  </div><div class="previewImagesPosted"><div class="previewImagePosted"><a href="'+itemUrl+'" target="_blank"><img src="'+itemImage+'" style="width: 130px; height: auto; float: left;"></a></div></div><div class="previewContentPosted"><div class="previewTitlePosted" ><a href="'+itemUrl+'" target="_blank"><span id="previewSpanTitle">'+itemTitle+'</span></a></div><div class="previewUrlPosted">'+itemCanonicalUrl+'</div><div class="previewDescriptionPosted"  > <span id="previewSpanDescription">'+itemDescription+'</span></div><div style="clear: both"></div></div>';
+                	var richComment = '<div class="previewPosted" style=""><div class="previewImagesPosted"><div class="previewImagePosted"><a href="'+itemUrl+'" target="_blank"><img src="'+itemImage+'" style="width: 130px; height: auto; float: left;"></a></div></div><div class="previewContentPosted"><div class="previewTitlePosted" ><a href="'+itemUrl+'" target="_blank"><span id="previewSpanTitle">'+itemTitle+'</span></a></div><div class="previewUrlPosted">'+itemCanonicalUrl+'</div><div class="previewDescriptionPosted"  > <span id="previewSpanDescription">'+itemDescription+'</span></div><div style="clear: both"></div></div>';
                 	opts.component.api(opts.objectID + '/comments', 'post', {
                 		"description": itemText,
                 		"extra": {
