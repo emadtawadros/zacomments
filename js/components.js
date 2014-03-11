1bf7be9c63e6368626ffc1d200d28788e7d85307
emadtawadros <emad.tawadros@gmail.com>
2014-03-11 10:14:43 -0700

    Update components.js

--- js/components.js
+++ js/components.js
@@ -171,6 +171,30 @@ Hull.component('posts', {
         
         Hull.component('test', {
             templates: ['test', 'form'],
+            helpers: {
+            	ifcond: function (v1, operator, v2, options) {
+            		switch (operator) {
+			        case '==':
+			            return (v1 == v2) ? options.fn(this) : options.inverse(this);
+			        case '===':
+			            return (v1 === v2) ? options.fn(this) : options.inverse(this);
+			        case '<':
+			            return (v1 < v2) ? options.fn(this) : options.inverse(this);
+			        case '<=':
+			            return (v1 <= v2) ? options.fn(this) : options.inverse(this);
+			        case '>':
+			            return (v1 > v2) ? options.fn(this) : options.inverse(this);
+			        case '>=':
+			            return (v1 >= v2) ? options.fn(this) : options.inverse(this);
+			        case '&&':
+			            return (v1 && v2) ? options.fn(this) : options.inverse(this);
+			        case '||':
+			            return (v1 || v2) ? options.fn(this) : options.inverse(this);
+			        default:
+			            return options.inverse(this);
+    			}
+		}
+            },
 
             refreshEvents: ['model.hull.me.change'],
             
