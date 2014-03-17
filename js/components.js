92c895967998d8dfd06e89ed8738056f80d2ae2b
emadtawadros <emad.tawadros@gmail.com>
2014-03-17 12:13:39 -0700

    Update components.js

--- js/components.js
+++ js/components.js
@@ -135,7 +135,7 @@ Hull.component('posts', {
             afterRender: function() {
                 var component = this;
                 
-		this.sandbox.on('hull.uploads.image.send', function(image) {                  
+		this.sandbox.on('hull.upload.image.add', function(image) {                  
 			console.log("Started image upload");                   
                 });
                 
