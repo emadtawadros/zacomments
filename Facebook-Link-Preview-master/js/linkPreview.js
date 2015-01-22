cd715644a6e0c9c2f326d3dcec80e7e67a8aeec8
emadtawadros <emad.tawadros@gmail.com>
2015-01-22 12:07:12 -0800

    filtering comments made by only current user

--- Facebook-Link-Preview-master/js/linkPreview.js
+++ Facebook-Link-Preview-master/js/linkPreview.js
@@ -518,12 +518,11 @@
                 	var start = opts.component.sandbox.util.moment().startOf('day').toDate().toISOString();
 			var end = opts.component.sandbox.util.moment().endOf('day').toDate().toISOString();
 			var currentUser = opts.component.sandbox.currentUser();
-			console.log(currentUser.id);
-			console.log(currentUser);
-			
+
                 	//See if the user already commented on this post today
                 	opts.component.api(opts.objectID + '/comments', 'get', {
                 		where: {
+                			user_id: currentUser.id,
                 			created_at: {
                 				'$gte': start,
                 				'$lte': end
