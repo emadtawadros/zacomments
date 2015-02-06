5300bab48f69ec9d8de057b6424893c9a5f01736
emadtawadros <emad.tawadros@gmail.com>
2015-02-06 15:43:59 -0800

    Updating LinkPreview to latest version 1.3

--- Facebook-Link-Preview-master/php/classes/LinkPreview.php
+++ Facebook-Link-Preview-master/php/classes/LinkPreview.php
@@ -4,7 +4,7 @@
  * Dual licensed under the MIT (http://www.opensource.org/licenses/mit-license.php)
  * and GPL (http://www.opensource.org/licenses/gpl-license.php) licenses.
  *
- * Version: 1.0.0
+ * Version: 1.3.0
  */
 
 /** Important php5-curl must be installed and enabled */
@@ -16,64 +16,15 @@ include_once "Url.php";
 include_once "Content.php";
 include_once "Json.php";
 
-class LinkPreview {
+class LinkPreview
+{
 
-    function __construct() {
+    function __construct()
+    {
     }
 
-    function getPage($url) {
-        $res = array();
-        $options = array(CURLOPT_RETURNTRANSFER => true, // return web page
-            CURLOPT_HEADER => false, // do not return headers
-            CURLOPT_FOLLOWLOCATION => true, // follow redirects
-            CURLOPT_USERAGENT => "spider", // who am i
-            CURLOPT_AUTOREFERER => true, // set referer on redirect
-            CURLOPT_CONNECTTIMEOUT => 120, // timeout on connect
-            CURLOPT_TIMEOUT => 120, // timeout on response
-            CURLOPT_MAXREDIRS => 10, // stop after 10 redirects
-        );
-        $ch = curl_init($url);
-        curl_setopt_array($ch, $options);
-        $content = curl_exec($ch);
-        $header = curl_getinfo($ch);
-        curl_close($ch);
-
-        $hrd = $header["content_type"];
-        header("Content-Type: ".$hrd, true);
-
-        $res['content'] = $content;
-        $res['url'] = $header['url'];
-        $res['header'] = $hrd;
-
-        return $res;
-    }
-
-    function getMedia($pageUrl){
-        $media = array();
-        if (strpos($pageUrl, "youtube.com") !== false) {
-            $media = Media::mediaYoutube($pageUrl);
-        } else if (strpos($pageUrl, "vimeo.com") !== false) {
-            $media = Media::mediaVimeo($pageUrl);
-        }
-        else if (strpos($pageUrl, "metacafe.com") !== false) {
-            $media = Media::mediaMetacafe($pageUrl);
-        }
-        else if (strpos($pageUrl, "dailymotion.com") !== false) {
-            $media = Media::mediaDailymotion($pageUrl);
-        }
-        else if (strpos($pageUrl, "collegehumor.com") !== false) {
-            $media = Media::mediaCollegehumor($pageUrl);
-        }
-        else if (strpos($pageUrl, "blip.tv") !== false) {
-            $media = Media::mediaBlip($pageUrl);
-        }
-        else if (strpos($pageUrl, "funnyordie.com") !== false) {
-            $media = Media::mediaFunnyordie($pageUrl);
-        }
-        return $media;
-    }
-
-    function joinAll($matching, $number, $url, $content) {
+    function joinAll($matching, $number, $url, $content)
+    {
         for ($i = 0; $i < count($matching[$number]); $i++) {
             $imgSrc = $matching[$number][$i] . $matching[$number + 1][$i];
             $src = "";
@@ -91,7 +42,8 @@ class LinkPreview {
         return $content;
     }
 
-    function crawl($text, $imageQuantity, $header){
+    function crawl($text, $imageQuantity, $header)
+    {
 
         if (preg_match(Regex::$urlRegex, $text, $match)) {
 
@@ -149,6 +101,13 @@ class LinkPreview {
                     $description = Content::crawlCode($raw);
                 }
 
+                if(Content::isJson($title)){
+                    $title = "";
+                }
+                if(Content::isJson($description)){
+                    $description = "";
+                }
+
                 $media = $this->getMedia($pageUrl);
                 $images = count($media) == 0 ? Content::extendedTrim($metaTags["image"]) : $media[0];
                 $videoIframe = $media[1];
@@ -169,12 +128,89 @@ class LinkPreview {
             $finalLink = explode("&", $finalUrl);
             $finalLink = $finalLink[0];
 
-            $answer = array("title" => $title, "titleEsc" => $title, "url" => $finalLink, "pageUrl" => $finalUrl, "canonicalUrl" => Url::canonicalPage($pageUrl), "description" => strip_tags($description), "descriptionEsc" => strip_tags($description), "images" => $images, "video" => $video, "videoIframe" => $videoIframe);
+            $description = strip_tags($description);
 
-            return Json::jsonSafe($answer, $header);
+            $answer = array("title" => $title, "url" => $finalLink, "pageUrl" => $finalUrl, "canonicalUrl" => Url::canonicalPage($pageUrl), "description" => $description,
+                "images" => $images, "video" => $video, "videoIframe" => $videoIframe);
+
+            $result_json = Json::jsonSafe($answer, $header);
+            $result_json_decoded = json_decode($result_json);
+
+            $flagged = false;
+
+            if (!isset($result_json_decoded->title)) {
+                $title = utf8_encode($title);
+                $flagged = true;
+            }
+
+            if (!isset($result_json_decoded->description)) {
+                $description = utf8_encode($description);
+                $flagged = true;
+            }
+
+            if ($flagged) {
+                $answer = array("title" => $title, "url" => $finalLink, "pageUrl" => $finalUrl, "canonicalUrl" => Url::canonicalPage($pageUrl), "description" => $description,
+                    "images" => $images, "video" => $video, "videoIframe" => $videoIframe);
+
+                return Json::jsonSafe($answer, $header);
+            } else {
+
+                return $result_json;
+            }
 
         }
         return null;
+    }
+
+    function getPage($url)
+    {
+        $res = array();
+        $options = array(CURLOPT_RETURNTRANSFER => true, // return web page
+            CURLOPT_HEADER => false, // do not return headers
+            CURLOPT_FOLLOWLOCATION => true, // follow redirects
+            CURLOPT_USERAGENT => "leocardz", // who am i
+            CURLOPT_AUTOREFERER => true, // set referer on redirect
+            CURLOPT_CONNECTTIMEOUT => 120, // timeout on connect
+            CURLOPT_TIMEOUT => 120, // timeout on response
+            CURLOPT_MAXREDIRS => 10, // stop after 10 redirects
+        );
+        $ch = curl_init($url);
+        curl_setopt_array($ch, $options);
+        $content = curl_exec($ch);
+        $header = curl_getinfo($ch);
+        curl_close($ch);
+
+        $hrd = $header["content_type"];
+        header("Content-Type: " . $hrd, true);
+
+        $res['content'] = $content;
+        $res['url'] = $header['url'];
+        $res['header'] = $hrd;
+
+        return $res;
+    }
+
+    function getMedia($pageUrl)
+    {
+        $media = array();
+        if (strpos($pageUrl, "youtube.com") !== false) {
+            $media = Media::mediaYoutube($pageUrl);
+        } else if (strpos($pageUrl, "vimeo.com") !== false) {
+            $media = Media::mediaVimeo($pageUrl);
+        } else if (strpos($pageUrl, "vine.co") !== false) {
+            $media = Media::mediaVine($pageUrl);
+        } else if (strpos($pageUrl, "metacafe.com") !== false) {
+            $media = Media::mediaMetacafe($pageUrl);
+        } else if (strpos($pageUrl, "dailymotion.com") !== false) {
+            $media = Media::mediaDailymotion($pageUrl);
+        } else if (strpos($pageUrl, "collegehumor.com") !== false) {
+            $media = Media::mediaCollegehumor($pageUrl);
+        } else if (strpos($pageUrl, "blip.tv") !== false) {
+            $media = Media::mediaBlip($pageUrl);
+        } else if (strpos($pageUrl, "funnyordie.com") !== false) {
+            $media = Media::mediaFunnyordie($pageUrl);
+        }
+        return $media;
     }
 
 }
