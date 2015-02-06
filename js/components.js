512a360980b7712074db1821588add1f8b0104e3
emadtawadros <emad.tawadros@gmail.com>
2015-02-05 16:28:15 -0800

    trying out sandbox.unflag

--- js/components.js
+++ js/components.js
@@ -132,6 +132,7 @@ Hull.component('posts', {
         	unflagItem: function (event, action) {
         		event.preventDefault();
         		var id = action.data.id;
+        		this.sandbox.unflag(id);
         		this.api(id + '/flag', 'delete').then(function(response) {
         			console.log(response);
         		});
