cfce23aa5b4b9643bd03a398e8a6c73249b9c6cb
emadtawadros <emad.tawadros@gmail.com>
2015-05-14 19:01:43 -0700

    Increasing limit to 6 months to fetch more trending
    For now

--- js/components.js
+++ js/components.js
@@ -4,7 +4,7 @@ Hull.component('posts', {
                 var tab = this.$el.parent().find('.loading');
                 tab.slideDown();
         
-                this.options.trendingDaysLimit = 60;    //3 days
+                this.options.trendingDaysLimit = 180;    //3 days
                 this.options.trendingLimit = 10;    //10 posts
             }, //initialize
             datasources: {
