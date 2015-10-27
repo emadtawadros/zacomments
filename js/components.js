dc97ec8d2b0160babfebbe35d6efcc9cb72a7601
emadtawadros <emad.tawadros@gmail.com>
2015-10-27 12:07:54 -0700

    confirm dialog to comments

--- js/components.js
+++ js/components.js
@@ -817,12 +817,32 @@ Hull.component('posts', {
             'keyup [name="description"]' : 'checkButtonStatus'
             },
             flagItem: function (event, action) {
+            	var component = this;
             	event.preventDefault();
             	var id = action.data.id;
-            	var isCertain = confirm('Do you want to report this content as inappropriate ?');
-            	if (isCertain) {
-            		this.sandbox.flag(id);
-            	}
+            	var n = noty({
+			text: 'Are you sure you want to report this comment? Please report only offensive and inappropriate comments.',
+			layout: 'topCenter',
+			theme: 'relax',
+			type: 'warning',
+			animation: {
+				open: {height: 'toggle'}, // jQuery animate function property object
+				close: {height: 'toggle'}, // jQuery animate function property object
+				easing: 'swing', // easing
+				speed: 300 // opening & closing animation speed
+			},
+			buttons: [
+				{addClass: 'btn btn-success', text: 'Yes', onClick: function($noty) {
+					$noty.close();
+					component.sandbox.flag(id);
+					}
+				},
+				{addClass: 'btn btn-default', text: 'I changed my mind', onClick: function($noty) {
+						$noty.close();
+					}
+				}
+			]
+		});
             },
             actions: {
             delete:  'deleteComment',
